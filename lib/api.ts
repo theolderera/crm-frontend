980aec5eecba3bd8f3d992b00ad592c48f0f7a42
theolderera <ahmadhayotov2511@gmail.com>
2026-06-05 18:41:40 +0500

    update 11fix3

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Group, Student, Attendance, AuthUser, AttendanceReport } from '@/types';
+import { Group, Student, Attendance, AuthUser, AttendanceReport, CourseMonth } from '@/types';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
